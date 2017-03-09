74076255c28a609a309f2edf5d4b7dc15bf25041
Hugo Nugra <hugo@alturasoluciones.com>
2017-03-09 17:13:18 -0500

    create routes and controller actions

--- app/controllers/collect_controller.rb
+++ app/controllers/collect_controller.rb
@@ -1,2 +1,32 @@
 class CollectController < ApplicationController
+
+	def landing_page
+  end
+
+  def save_mail
+  	email = params[:email]
+  	redirect_to action: "register_volunteer_form", volunteer_email: email
+  end
+
+  def register_volunteer_form
+  	@volunteer_email = params[:volunteer_email]
+  end
+
+  def register_volunteer_save
+  	@volunteer = params[:user]
+  	@volunteer.save
+  end
+
+   def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
