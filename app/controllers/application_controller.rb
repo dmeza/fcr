48ebb4f1aa6b70dce721f7de5129a7e480b4f01f
Gustavo Gonzalez <gustavo@alturasoluciones.com>
2017-07-25 18:45:23 -0500

    Show history field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:email, :first_name, :last_name, :indentifier, :city_id, :phone, :mobile, :birth_date, :motivation, :password, :password_confirmation, :contact_me, :has_institution_induction)
+      u.permit(:email, :first_name, :last_name, :indentifier, :city_id, :phone, :mobile, :birth_date, :motivation, :history, :password, :password_confirmation, :contact_me, :has_institution_induction)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:email, :first_name, :last_name, :indentifier, :city_id, :phone, :mobile, :birth_date, :motivation, :password, :password_confirmation, :contact_me, :has_institution_induction, :remember_me, :current_password)
+      u.permit(:email, :first_name, :last_name, :indentifier, :city_id, :phone, :mobile, :birth_date, :motivation, :history, :password, :password_confirmation, :contact_me, :has_institution_induction, :remember_me, :current_password)
     end
     #devise_parameter_sanitizer.for(:invite).concat [:role_id, :company_id]
     #devise_parameter_sanitizer.for(:accept_invitation).concat [:first_name, :last_name]
