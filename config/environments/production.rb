42546d1104b413a0de57178b2b841b2397b6395f
Gustavo Gonzalez <gustavo@alturasoluciones.com>
2019-06-17 20:56:47 -0500

    Update email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'uio.acolita.org' }
+  config.action_mailer.default_url_options = { :host => 'imparables.ec' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = YAML.load_file(Rails.root.join('config', 'email_settings.yml')).symbolize_keys
 
