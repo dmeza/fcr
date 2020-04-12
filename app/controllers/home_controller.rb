1a2492c6461d6e6512a53035a08f4bfa9fa343bc
Gustavo Gonzalez <gustavo@alturasoluciones.com>
2020-04-11 20:43:20 -0500

    Add national option

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,16 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :calendar]
 
   def index
-    @city_id = (params[:city_id].nil? ? 1 : params[:city_id])
-    @events = Event.active.city(@city_id).paginate(page: params[:page], per_page: 50)
+    @city_id = (params[:city_id].nil? || params[:city_id] == "" ? 0 : params[:city_id])
+    @events = Event.active.paginate(page: params[:page], per_page: 50)
+    @events = @events.city(@city_id) unless @city_id == 0
     render :home if current_user
   end
 
   def calendar
-    @city_id = (params[:city_id].nil? ? 1 : params[:city_id])
-    @events = Event.active.city(@city_id).after(params['start']).before(params['end'])
+    @city_id = (params[:city_id].nil? || params[:city_id] == "" ? 0 : params[:city_id])
+    @events = Event.active.after(params['start']).before(params['end'])
+    @events = @events.city(@city_id) unless @city_id == 0
 
     #@events = Event.all
     #@events = @events.after(params['start']) if (params['start'])
